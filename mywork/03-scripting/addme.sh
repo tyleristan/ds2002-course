538e10ea8a95a8cf40b1088be54a1ab23323ad58
Tyler Tan <twg2nk@virginia.edu>
2024-02-08 15:36:41 -0500

    testing

---
+++ mywork/03-scripting/addme.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+clear
+read -p "What is the first number you want me to add? " FIRNUM 
+read -p "What is the second number you want me to add? " SECNUM
+
+SUMOF=$FIRNUM+$SECNUM
+
+echo "The sum is $SUMOF"
---
+++ mywork/03-scripting/readme.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+# This script is designed to _____
+# Author: Tyler Tan <twg2nk@virginia.edu?
+# Last updated: 2/6/24
+
+read -p "What is your favorite number?" FAVNUM
+echo "Hey $USERNAME I heard your favorite number is $FAVNUM"
+
